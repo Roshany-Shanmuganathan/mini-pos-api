3f21d802c8e5235dd6c60515bd8cf31fa8c854bf
Roshany Shanmuganathan <s.roshany7@gmail.com>
2025-10-17 05:18:57 +0530

    item model modified

--- models/Item.js
+++ models/Item.js
@@ -2,6 +2,11 @@ import mongoose from "mongoose";
 
 const ItemSchema = new mongoose.Schema({
   name: { type: String, required: true, trim: true },
+  sku: { type: String, unique: true, required: true },
+  description: { type: String, required: true },
+  price: { type: Number, required: true, min: 0 },
+  stock: { type: Number, default: 0 },
+  description: { type: String, required: true },
 
   createdAt: {
     type: Date,
